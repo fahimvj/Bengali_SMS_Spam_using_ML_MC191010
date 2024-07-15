82d0367212d7f32f412cfd8c9dec6d4c0c0ac7da
MD Fahim Shahriar Chowdhury <5455584+fahimvj@users.noreply.github.com>
2024-07-15 13:19:01 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,10 @@ st.markdown('<p><strong>MD Fahim Shahriar Chowdhury, ID-MC191010</strong></p>', 
 
 input_sms = st.text_input("Enter the SMS Text ")
 
-if st.button("Predict"):
-    if input_sms.strip() != "":
-
-        #1 Process
+ if input_sms.strip() != "":
+     st.error("Please enter a valid SMS text.")
+ else:
+            #1 Process
 
         #cleaning text
         
@@ -49,5 +49,5 @@ if st.button("Predict"):
         else:
           st.header("\n Not Spam (এই মেসেজ টি সম্ভবত স্প্যাম নই। )")
 
-else:
- st.error("Please enter a valid SMS text.")
+
+ 
