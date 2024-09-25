f1ae86fc429664cfb2720eb667360bb80ceb675f
MD Fahim Shahriar Chowdhury <5455584+fahimvj@users.noreply.github.com>
2024-09-25 09:46:12 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,18 +11,7 @@ tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
 
-st.markdown('<p>&nbsp;</p>', unsafe_allow_html=True)
-st.markdown('<h3 style="text-align: center;"><strong>A Project Proposal on</strong></h3>', unsafe_allow_html=True)
 st.markdown('<h4 style="text-align: center;"><strong>BANGLA SMS SPAM DETECTION USING MACHINE LEARNING</strong></h4>', unsafe_allow_html=True)
-st.markdown('<p>&nbsp;</p>', unsafe_allow_html=True)
-
-st.markdown('<h5><strong>Supervised by</strong></h5>', unsafe_allow_html=True)
-st.markdown('<p><strong>Mohammed Mahmudur Rahman</strong></p>', unsafe_allow_html=True)
-st.markdown('<p>Associate Professor, Dept of CSE, IIUC</p>', unsafe_allow_html=True)
-
-st.markdown('<h5><strong>Submitted by</strong></h5>', unsafe_allow_html=True)
-st.markdown('<p>MD Fahim Shahriar Chowdhury, ID-MC191010</p>', unsafe_allow_html=True)
-
 
 
 input_sms = st.text_input("Enter the SMS Text ")
